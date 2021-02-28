c3a68de75d8546b67adb540902844678841a2b6a
Angelo Zambrano <angelozam17@gmail.com>
2021-02-28 09:54:08 -0500

    feat: add styles setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import * as React from "react";
+
+/* Styles */
+import "normalize.css";
+import { globals } from "styles";
+import { Global } from "@emotion/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Global styles={globals} />
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/globals.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+export const globals = css`
+  body {
+    font-family: "Inter", sans-serif;
+  }
+`;
+
+export default globals;
