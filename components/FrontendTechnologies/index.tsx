91177c4a8a11c593d00029b8659bf5d6c366eafb
Angelo Zambrano <angelozam17@gmail.com>
2021-04-11 21:44:52 -0500

    feat: add showmore button

--- components/FrontendTechnologies/index.tsx
+++ components/FrontendTechnologies/index.tsx
@@ -10,18 +10,25 @@ import { frontendTechnologies as technologies } from "fixtures";
 import { Container, Item, IconContainer, Text } from "./technologies.styles";
 
 function FrontendTechnologies() {
+  const [showMore, setShowMore] = React.useState(false);
+
   return (
     <SectionLayout title="Frontend Technologies.">
       <Container>
-        {technologies.map(({ Icon, color, title, href }) => (
-          <Item className="border-color-gray-100 dark-border-color-gray-700">
-            <IconContainer target="_blank" href={href}>
-              <Icon width="5rem" fill={color} />
-            </IconContainer>
-            <Text className="color-black dark-color-white">{title}</Text>
-          </Item>
-        ))}
+        {technologies
+          .slice(0, showMore ? 6 : technologies.length)
+          .map(({ Icon, color, title, href }) => (
+            <Item className="border-color-gray-100 dark-border-color-gray-700">
+              <IconContainer target="_blank" href={href}>
+                <Icon width="5rem" fill={color} />
+              </IconContainer>
+              <Text className="color-black dark-color-white">{title}</Text>
+            </Item>
+          ))}
       </Container>
+      <button onClick={() => setShowMore(!showMore)}>
+        {showMore ? "Show more" : "Show less"}
+      </button>
     </SectionLayout>
   );
 }
