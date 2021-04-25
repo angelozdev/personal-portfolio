22f486ed97af9414faeceb650f4d325b304be910
Angelo Zambrano <angelozam17@gmail.com>
2021-04-25 14:06:49 -0500

    feat: finish footer section

---
+++ components/Footer/footer.styles.ts
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import { breakpoints } from "styles/theme";
+
+export const Container = styled.footer`
+  padding: 3rem 0;
+  transition: var(--transition-base) background-color;
+  border-top: 1px solid;
+`;
+
+export const Content = styled.div``;
+
+export const Grid = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media (max-width: ${breakpoints.sm}) {
+    justify-content: center;
+  }
+`;
+
+export const Pane = styled.div`
+  text-align: right;
+  flex-basis: 300px;
+
+  @media (max-width: ${breakpoints.sm}) {
+    text-align: center;
+  }
+`;
+
+export const Title = styled.h2`
+  margin-top: 0;
+`;
+
+export const Text = styled.p`
+  margin-bottom: 0;
+`;
+
+export const Anchor = styled.a`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
--- components/index.ts
+++ components/index.ts
@@ -4,6 +4,7 @@ export { default as Button } from "./Button";
 export { default as Card } from "./Card";
 export { default as Contact } from "./Contact";
 export { default as Divider } from "./Divider";
+export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as Hero } from "./Hero";
 export { default as Main } from "./Main";
