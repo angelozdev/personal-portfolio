c2b13930a910028c4c73fbc64adf25d8493b8057
Angelo Zambrano <angelozam17@gmail.com>
2021-07-25 13:59:40 -0500

    :zap: perf(avatar): use image from nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,10 +15,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;700;900&display=swap"
-          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=optional"
+          rel="preload"
+          as="style"
         />
       </Head>
       <DarkModeProvider>
